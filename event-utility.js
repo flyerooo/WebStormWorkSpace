b855a07a1c5bec2bdeca0372b58b6315ac553853
JeffZzz <wxl.pure@gmail.com>
2016-09-12 19:37:28 +0800

    Update event-utility.js

--- event-utility.js
+++ event-utility.js
@@ -1,9 +1,32 @@
+/**
+ * Created by ibm on 2016/9/12.
+ */
 var evt = {
-    addListener:function (obj, type, fn) {
+    addListener: function (obj, type, fn) {
         if (obj.addEventListener) {
             obj.addEventListener(type, fn);
         } else {
+            obj.attachEvent("on", type, fn)
+        }
+    },
+    removeListener: function (obj, type, fn) {
+        if (obj.removeListener) {
+         obj.removeListener(type, fn);
+        } else {
             obj.attachEvent("on" + type, fn);
         }
+    },
+    getTarget: function (e) {
+        if (e.target) {
+            return e.target;
+        }
+        return e.srcElement;
+    },
+    preventDefault:function (e) {
+        if (e.preventDefault) {
+            e.preventDefault();
+        } else {
+            e.returnValue = false;
+        }
     }
-}
+};
